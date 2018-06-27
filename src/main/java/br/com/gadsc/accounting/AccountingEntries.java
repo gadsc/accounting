95b6d6cd527fb421a6a096ec90493f0435dbfda4
Gabz <gadsc@hotmail.com>
2018-06-27 00:30:42 -0300

    adiciona log para quando é executada uma inserção de conta contábil

--- src/main/java/br/com/gadsc/accounting/AccountingEntries.java
+++ src/main/java/br/com/gadsc/accounting/AccountingEntries.java
@@ -10,11 +10,13 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 class AccountingEntries {
-	
+	private final Logger LOGGER = LoggerFactory.getLogger(AccountingEntries.class);
 	private final Map<UUID, AccountingEntry> entries = new ConcurrentHashMap<>();
 	private final Map<Long, List<UUID>> keys = new ConcurrentHashMap<>();
 	
@@ -22,11 +24,14 @@ class AccountingEntries {
 		UUID uuid = UUID.randomUUID();
 		
 		entries.put(uuid, accountingEntry);
+		LOGGER.info("Accounting entry with account number " + accountingEntry.getAccountNumber() + " successfully inserted with id: " + uuid);
 		
 		if (keys.containsKey(accountingEntry.getAccountNumber())) {
 			keys.get(accountingEntry.getAccountNumber()).add(uuid);
+			LOGGER.debug("Key of accounting number " + accountingEntry.getAccountNumber() + " updated successfully.");
 		} else {
-			keys.put(accountingEntry.getAccountNumber(), new ArrayList<>(Arrays.asList(uuid)));			
+			keys.put(accountingEntry.getAccountNumber(), new ArrayList<>(Arrays.asList(uuid)));
+			LOGGER.debug("Key of accounting number " + accountingEntry.getAccountNumber() + " inserted successfully.");
 		}
 		
 		return uuid;
